b32798674bfbf18a7a6e6537db4c0a8bd219ff83
Matthew Seguin <seguin63@uwindsor.ca>
2026-03-15 21:57:54 +0000

    made final fixes to game logic including fixed collision detection, fixed win condition, fixed enemy movement and fixed enemy logic. game is complete!

--- SpaceShooter.js
+++ SpaceShooter.js
@@ -5,6 +5,7 @@ let score = 0;
 let enemyDirection = 1;
 let enemyShouldDrop = false;
 let playerLives = 3;
+let reset = false;
 
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
@@ -16,7 +17,7 @@ const assets = {
     life: new Image()
 };
 
-function loadAssets(){
+function loadAssets() {
     return Promise.all([
         new Promise(res => { assets.player.onload = res; assets.player.src = 'player.png'; }),
         new Promise(res => { assets.enemy.onload = res; assets.enemy.src = 'enemyShip.png'; }),
@@ -29,7 +30,7 @@ const keys = {};
 window.addEventListener('keydown', (e) => {
     keys[e.code] = true;
 
-    if(["Space", "ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"].includes(e.code)){
+    if (["Space", "ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"].includes(e.code)) {
         e.preventDefault();
     }
 });
@@ -63,62 +64,62 @@ class Entity {
 }
 
 class Player extends Entity {
-    constructor(x, y, width, height, image){
+    constructor(x, y, width, height, image) {
         super(x, y, width, height, image);
         this.speed = 15;
         this.cooldown = 0;
     }
 
-    update(){
-        if(keys['ArrowLeft'] && this.x > 0){
+    update() {
+        if (keys['ArrowLeft'] && this.x > 0) {
             this.x -= this.speed;
         }
 
-        if(keys['ArrowRight'] && this.x < canvas.width - this.width){
+        if (keys['ArrowRight'] && this.x < canvas.width - this.width) {
             this.x += this.speed;
         }
 
-        if(keys['Space'] && this.cooldown <= 0){
+        if (keys['Space'] && this.cooldown <= 0) {
             this.fire()
             this.cooldown = 30;
         }
 
-        if(this.cooldown > 0){
+        if (this.cooldown > 0) {
             this.cooldown--;
         }
     }
 
-    fire(){
+    fire() {
         const X = this.x + this.width / 2 - 4;
         const Y = this.y;
 
         const newLaser = new Laser(X, Y, 9, 37, assets.laser);
         lasers.push(newLaser);
-    } 
+    }
 }
 
 class Laser extends Entity {
-    constructor(x, y, width, height, image){
+    constructor(x, y, width, height, image) {
         super(x, y, width, height, image);
         this.speed = 10;
     }
 
-    update(){
+    update() {
         this.y -= this.speed;
 
-        if(this.y + this.height < 0){
+        if (this.y + this.height < 0) {
             this.destroyed = true;
         }
     }
 }
 
 class Enemy extends Entity {
-    constructor(x, y, width, height, image){
+    constructor(x, y, width, height, image) {
         super(x, y, width, height, image);
         this.speed = 1.5;
     }
 
-    update(direction){
+    update(direction) {
         this.x += this.speed * direction;
     }
 }
@@ -127,11 +128,11 @@ function createEnemies() {
     const enemyTotal = 8;
     const enemySpacing = 80;
     const totalWidth = enemyTotal * enemySpacing;
-    const X = (canvas.x - totalWidth) / 2;
+    const X = (canvas.width - totalWidth) / 2;
 
-    for(let row = 0; row < 3; row++){
-        for(let col = 0; col < enemyTotal; col++){
-            const x = X + col * enemySpacing + 50;
+    for (let row = 0; row < 3; row++) {
+        for (let col = 0; col < enemyTotal; col++) {
+            const x = X + col * enemySpacing;
             const y = row * 80 + 50;
 
             const enemy = new Enemy(x, y, 98, 50, assets.enemy);
@@ -149,7 +150,7 @@ function resetEnemies() {
     createEnemies();
 }
 
-function updateGame(){
+function updateGame() {
     player.update();
 
     lasers.forEach(laser => laser.update());
@@ -160,14 +161,14 @@ function updateGame(){
     checkCollisions();
     gameOver();
 
-    function updateEnemies(){
+    function updateEnemies() {
         enemies.forEach(enemy => {
-            if(enemy.x + enemy.width > canvas.width || enemy.x < 0){
+            if (enemy.x + enemy.width > canvas.width || enemy.x < 0) {
                 enemyShouldDrop = true;
             }
         });
 
-        if(enemyShouldDrop){
+        if (enemyShouldDrop) {
             enemyDirection *= -1;
             enemies.forEach(enemy => enemy.y += 40);
             enemyShouldDrop = false;
@@ -176,40 +177,52 @@ function updateGame(){
         enemies.forEach(enemy => enemy.update(enemyDirection));
     }
 
-    function checkCollisions(){
-        lasers.forEach(laser => {
-            enemies.forEach(enemy => {
-                if(laser.intersects(enemy)){
+    function checkCollisions() {
+        for (let laser of lasers) {
+            for (let enemy of enemies) {
+                if (laser.intersects(enemy)) {
                     laser.destroyed = true;
                     enemy.destroyed = true;
-                    score += 1;
-                    return;
+                    score++;
+                    break;
                 }
-            });
-        });
-
-        enemies.forEach(enemy => {
-            if(enemy.intersects(player)){
-                playerLives--;
-                setTimeout(resetEnemies, 1000);
             }
-        });
+        }
+
+        for (let enemy of enemies) {
+            if ((enemy.intersects(player) || enemy.y + enemy.height >= canvas.height) && !reset) {
+                reset = true;
+                playerLives--;
+
+                enemies = [];
+
+                resetEnemies();
+                reset = false;
+
+                break;
+            }
+        }
     }
 
-    function gameOver(){
-        if(enemies.length === 0){
+    function gameOver() {
+
+        if (score >= 100) {
             alert("You Win! Score: " + score);
             window.location.reload();
         }
 
-        if(playerLives <= 0){
+        if (enemies.length === 0) {
+            resetEnemies();
+        }
+
+        if (playerLives <= 0) {
             alert("The aliens invaded! Game Over!");
             window.location.reload();
         }
     }
 }
 
-function drawGame(){
+function drawGame() {
     ctx.fillStyle = 'black';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
@@ -221,22 +234,22 @@ function drawGame(){
     ctx.font = '20px Arial';
     ctx.fillText(`Score: ${score}`, 20, 30);
 
-    for(let i = 0; i < playerLives; i++){
+    for (let i = 0; i < playerLives; i++) {
         const lifeX = canvas.width - 150 + (i * 30);
         ctx.drawImage(assets.life, lifeX, 10, 30, 30);
     }
 }
 
-function gameLoop(){
+function gameLoop() {
     updateGame();
     drawGame();
     requestAnimationFrame(gameLoop);
 }
 
-window.onload = async() => {
+window.onload = async () => {
     await loadAssets();
 
-    player = new Player(canvas.width/2-45, canvas.height-100, 99, 75, assets.player);
+    player = new Player(canvas.width / 2 - 45, canvas.height - 100, 99, 75, assets.player);
     createEnemies();
 
     gameLoop();
